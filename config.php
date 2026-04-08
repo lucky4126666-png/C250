0e3d88ae7ebc569fda9ad4e232e190691ce8ebeb
Nguyen Long <lucky4126666@gmail.com>
2026-04-08 08:13:07 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+return array (
+  'mysql' => 
+  array (
+    'host' => '127.0.0.1',
+    'port' => '3306',
+    'user' => '123',
+    'password' => '123',
+    'database' => '123',
+  ),
+  'redis' => 
+  array (
+    'host' => '127.0.0.1',
+    'port' => '6379',
+    'password' => '',
+  ),
+  'tg_token' => '123',
+  'owner_id' => '123',
+  'domain' => 'https://123',
+);
